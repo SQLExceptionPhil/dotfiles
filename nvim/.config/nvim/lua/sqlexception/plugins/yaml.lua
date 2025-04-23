b55d7302c09dd42bcc53306853ad89a015acbc90
Philipp Hochmann <philipp.hochmann@gmx.at>
2025-04-23 16:36:34 +0200

    feat: add yamlls, python venv switcher

---
+++ nvim/.config/nvim/lua/sqlexception/plugins/yaml.lua
@@ -0,0 +1,32 @@
+return {
+	{
+		"cuducos/yaml.nvim",
+		ft = { "yaml", "yml" }, -- optional
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"folke/snacks.nvim", -- optional
+			"nvim-telescope/telescope.nvim", -- optional
+			"ibhagwan/fzf-lua", -- optional
+		},
+		keys = {
+			{ "<leader>yt", ":YAMLTelescope<CR>", desc = "Find yaml key/value using telescope" },
+			{ "<leader>yl", "!yamllint %<CR>", desc = "Lint yaml file" },
+		},
+	},
+	{
+		{
+			"someone-stole-my-name/yaml-companion.nvim",
+			dependencies = {
+				{ "neovim/nvim-lspconfig" },
+				{ "nvim-lua/plenary.nvim" },
+				{ "nvim-telescope/telescope.nvim" },
+			},
+			config = function()
+				require("telescope").load_extension("yaml_schema")
+			end,
+			keys = {
+				{ "<leader>ys", ":Telescope yaml_schema<CR>", desc = "Change yaml_schema" },
+			},
+		},
+	},
+}
